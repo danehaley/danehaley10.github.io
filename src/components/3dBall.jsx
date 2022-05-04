626b5c685aa99a826d932aff13e539013f729e00
Dane Dobra <danehaley22@gmail.com>
2022-05-04 15:42:15 +1000

    First completed implementation of front page

---
+++ src/components/3dBall.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Sphere, MeshDistortMaterial } from "@react-three/drei";
+
+export default function Ball() {
+  return (
+    <Sphere visible args={[1.2, 32, 64]} scale={2}>
+      <MeshDistortMaterial
+        color="white"
+        attach="material"
+        distort={0.3}
+        speed={1}
+        roughness={0.8}
+      />
+    </Sphere>
+  );
+}
--- src/pages/welcome.jsx
+++ src/pages/welcome.jsx
@@ -1,13 +1,39 @@
-import myHero from "../assets/img/Welcome/myHero.png";
+import React from "react";
+import puppy from "../assets/img/Welcome/puppy.png";
+import tree from "../assets/img/Welcome/tree.png";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import Ball from "../components/3dBall";
 import "../styles/welcome.scss";
 
 function Welcome() {
   return (
     <main className="grid-container">
-      <img src={myHero} alt="A puppy." />
-      <div className="text-container">
-        <h1>Dane Haley</h1>
-        <h2>Web Developer</h2>
+      <div className="link-container">
+        <p>skills</p>
+        <p>projects</p>
+        <p>education</p>
+        <p>resume</p>
+        <p>contact</p>
+      </div>
+      <div className="media-container">
+        <Canvas className="canvas">
+          <ambientLight intensity={0.5} />
+          <directionalLight position={[-2, 5, 2]} intensity={1} />
+          <Ball />
+        </Canvas>
+        <div className="images">
+          <img className="full-image" src={puppy} alt="A puppy." />
+          <img
+            className="shorter-image margin-l-10"
+            src={tree}
+            alt="A tree drawn via 'stick and poke' technique."
+          />
+        </div>
+        <div className="text">
+          <h1>Dane Haley</h1>
+          <h2>Web Developer</h2>
+        </div>
       </div>
     </main>
   );
