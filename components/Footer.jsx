16036bdcceb00a817af50b202b2cddebb1aa9df5
Dane Dobra <danehaley22@gmail.com>
2022-05-20 18:16:43 +1000

    - Implementation of technology stack - First implementation of the home page

---
+++ components/Footer.jsx
@@ -0,0 +1,14 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-wrapper footer-wrapper--left">
+        <a className="footer__item--link">Resume</a>
+        <a className="footer__item--link">Contact</a>
+        <a className="footer__item--link">Source</a>
+      </div>
+      <div className="footer-wrapper footer-wrapper--right">
+        <p className="footer__item--base">Dane Dobra 🔩 2022</p>
+      </div>
+    </footer>
+  );
+}
---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import styles from "../styles/app.scss";
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
